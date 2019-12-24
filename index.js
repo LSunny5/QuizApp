8dea017402c54790d0c013b68b80640f46cd57a8
Sunny Lee <LeeSunny507@gmail.com>
2019-12-24 01:34:01 -0500

    added questions, answers, responses, to the store.js

---
+++ index.js
@@ -0,0 +1,23 @@
+
+
+
+
+
+//Beginning of quiz
+function startPage() {
+
+
+    $('.startPage').on('click', '.startButton', function(event) {
+        
+    });
+}
+
+
+//Javascript Functions to run
+function begin() {
+    startPage();
+
+
+}
+
+$(begin);
---
+++ store.js
@@ -0,0 +1,134 @@
+//Questions for the Princess Quiz Storage
+const STORE = [
+    //Question 1
+    {
+        question: 'What is the name of the raccoon that follows Pocahontas?', 
+        answers: [
+            'Meeko', 'Bandit', 'Rocket', 'Kit'
+        ], 
+        correct: 'Meeko'
+    }
+    //Question 2
+    {
+        question: 'Which princess has a job (not including her Princess duties)?', 
+        answers: [
+            'Elsa', 'Cinderella', 'Tiana', 'Belle'
+        ], 
+        correct: 'Tiana'
+    }
+    //Question 3
+    {
+        question: 'Anna finishes the phrase "We finish each other\'s..." with?', 
+        answers: [
+            'Sentences', 'Sandwiches', 'Songs', 'Salads'
+        ], 
+        correct: 'Sandwiches'
+    }
+    //Question 4
+    {
+        question: 'What does the name Mulan mean?', 
+        answers: [
+            'Warrior', 'Chosen', 'Beautiful', 'Blossom'
+        ], 
+        correct: 'Blossom'
+    }
+    //Question 5
+    {
+        question: 'Which princess has brothers?', 
+        answers: [
+            'Moana', 'Merida', 'Snow White', 'Pocahontas'
+        ], 
+        correct: 'Merida'
+    }
+    //Question 6
+    {
+        question: 'Who is the first princess with powers?', 
+        answers: [
+            'Snow White', 'Elsa', 'Aurora', 'Rapunzel'
+        ], 
+        correct: 'Rapunzel'
+    }
+    //Question 7
+    {
+        question: 'This princess does not wear a dress.', 
+        answers: [
+            'Jasmine', 'Rapunzel', 'Tiana', 'Pocahontas'
+        ], 
+        correct: 'Jasmine'
+    }
+    //Question 8
+    {
+        question: 'Who is the youngest princess?', 
+        answers: [
+            'Anna', 'Jasmine', 'Snow White', 'Ariel'
+        ], 
+        correct: 'Snow White'
+    }
+    //Question 9
+    {
+        question: 'Which princess has violet eyes?', 
+        answers: [
+            'Tinkerbell', 'Belle', 'Aurora', 'Jasmine'
+        ], 
+        correct: 'Aurora'
+    }
+    //Question 10
+    {
+        question: 'What is the name of Belle\'s horse?', 
+        answers: [
+            'Philippe', 'Maximus', 'Khan', 'Samson'
+        ], 
+        correct: 'Philippe'
+    }
+    //Question 11
+    {
+        question: 'What is the Tinkerbell\'s sister\'s name?', 
+        answers: [
+            'Silvermist', 'Periwinkle', 'Fawn', 'Iridessa'
+        ], 
+        correct: 'Periwinkle'
+    }
+    //Question 12
+    {
+        question: 'What is the name of Cinderella\'s stepmother?', 
+        answers: [
+            'Olga', 'Narissa', 'Gothel', 'Tremaine'
+        ], 
+        correct: 'Tremaine'
+    }
+    //Question 13
+    {
+        question: 'What is the name of the giant snow monster Else created?', 
+        answers: [
+            'Snow', 'Marshmallow', 'Snowgie', 'Sentry'
+        ], 
+        correct: 'Marshmallow'
+    }
+    //Question 14
+    {
+        question: 'How many sisters does Ariel have?', 
+        answers: [
+            '7', '5', '8', '6'
+        ], 
+        correct: '6'
+    }
+    //Question 15
+    {
+        question: 'Which princess does not sing a song?', 
+        answers: [
+            'Aurora', 'Mulan', 'Merida', 'Snow White'
+        ], 
+        correct: 'Merida'
+    }
+];
+
+
+
+
+
+
+
+
+
+
+
