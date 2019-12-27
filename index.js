5dbf69da27fd9af8e0e9b0d5be5a43269f886781
Sunny Lee <leesunny507@gmail.com>
2019-12-27 02:31:22 -0500

    css edits to question page

--- index.js
+++ index.js
@@ -4,31 +4,37 @@ let qNumber=0;
 
 
 
-//Beginning of quiz
+//Function for when user clicks the start button
 function startPage() {
     $('.startQuiz').on('click', '.startButton', function(event){
-        //console.log("the button was clicked");
         $('.startQuiz').hide();
         $('.questionPage').removeClass('hide');
-        showQuestion(STORE[qNumber]);
+        $('#questionNumber').html(++qNumber);
+        $('#userScore').html(score);
+        showQuestion(STORE[qNumber-1]);
 
         
     });
 }
 
+//Function for going to next question
+//function for going to quiz completion page
+//reset the quiz after button is pressed
 
 
 
 //question layout
 function showQuestion (questionObject) {
-    console.log(questionObject.question);
+   // console.log(questionObject.question);
     $('.question').text(questionObject.question);    
-
-
+    $('#choiceA').value(questionObject.answers[0]);
+   // $('#choiceB').text(questionObject.answers[1]);
+   // $('#choiceC').text(questionObject.answers[2]);
+   // $('#choiceD').text(questionObject.answers[3]);
+    
 
     //qNumber++;
 }
-
 
 
 
