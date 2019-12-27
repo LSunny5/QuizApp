7ff6d8dbe8c313ec980abedaba0c17782055bd82
Sunny Lee <leesunny507@gmail.com>
2019-12-27 03:10:16 -0500

    buttons have values from store

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ let qNumber=0;
 //Function for when user clicks the start button
 function startPage() {
     $('.startQuiz').on('click', '.startButton', function(event){
+
         $('.startQuiz').hide();
         $('.questionPage').removeClass('hide');
         $('#questionNumber').html(++qNumber);
@@ -26,11 +27,13 @@ function startPage() {
 //question layout
 function showQuestion (questionObject) {
    // console.log(questionObject.question);
-    $('.question').text(questionObject.question);    
-    $('#choiceA').value(questionObject.answers[0]);
-   // $('#choiceB').text(questionObject.answers[1]);
-   // $('#choiceC').text(questionObject.answers[2]);
-   // $('#choiceD').text(questionObject.answers[3]);
+    $('.question').text(questionObject.question);  
+    $('#choiceA').html(questionObject.answers[0]);  
+    
+    //$('#choiceA').text(questionObject.answers[0]);
+    $('#choiceB').text(questionObject.answers[1]);
+    $('#choiceC').text(questionObject.answers[2]);
+    $('#choiceD').text(questionObject.answers[3]);
     
 
     //qNumber++;
