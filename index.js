cba6e6ca51ff61078292c1c5a5210bcf83051fba
Sunny Lee <leesunny507@gmail.com>
2019-12-27 23:09:48 -0500

    quiz is functioning until checking answer

--- index.js
+++ index.js
@@ -3,56 +3,106 @@ let score=0;
 let qNumber=0;
 
 
+//initialize the quiz
+function initializeQuiz() {
+    $('.questionPage').hide();
+    $('.finalPage').hide();
+    let score = 0;
+    let qNumber = 0;
+}
 
 //Function for when user clicks the start button
 function startPage() {
     $('.startQuiz').on('click', '.startButton', function(event){
         $('.startQuiz').hide();
-        $('.questionPage').removeClass('hide');
-        $('#questionNumber').html(++qNumber);
-        $('#userScore').html(score);
-        showQuestion(STORE[qNumber-1]);
+        $('.questionPage').show();
+        checkQuizNumber();
     });
 }
 
-//Function for going to next question
-//function for going to quiz completion page
-//reset the quiz after button is pressed
+//check to see if quiz is completed or continuing
+function checkQuizNumber () {
+    if (qNumber < STORE.length) {
+        return questionForm(qNumber);
+    }else{
+        console.log("quiz is done");
 
 
 
-//question layout
-function showQuestion (questionObject) {
+
+
+
+
+       // $('.questionPage').hide();
+       // finalResult();
+       // $('.qNumber').text(15);
+    }
+}
+
+//print out each question
+function questionForm(questionNo) {
+    let createQuestion = 
+        `<form>
+            <fieldset>
+                <legend>Question</legend>
+                <p><span id="questionNumber">${questionNo+1}</span> out of 15</p>
+                <p class="question">${STORE[questionNo].question}</p>
+                <section class="buttonChoices"></section>
+                <p class="scoreText">Score: <span id="userScore">${score}</span> out of 15</p>
+                <button type="submit" id="submitQuestion">Submit</button>
+            </fieldset>
+        </form>`
+
+    $('.questionPage').append(createQuestion); 
+
+    STORE[questionNo].answers.forEach(function (answerValue, answerIndex) {
+        $(`<label for="${answerIndex} class="result"></label>
+        <input type="radio" class="choice" name="qChoices" id="${answerIndex}" value="${answerValue}" required>
+        <span>${answerValue}</span>`).appendTo('.buttonChoices');
+    });
+    checkAnswer();
+}
+
+//check if answer is right after submitting
+function checkAnswer(){
+    $('#submitQuestion').on('click', function(event) {
+        event.preventDefault();
+        let picked = $('input:checked');
+        let userAnswer = picked.val();
+        let correctChoice = STORE[qNumber].correct;
+        if (userAnswer === correctChoice) {
+            console.log("correct");
+        } else {
+            console.log("wrong");
+        };
 
 
     
-   // console.log(questionObject.question);
-    $('.question').text(questionObject.question);  
-    $('#choiceA').html(questionObject.answers[0]);  
     
-    //$('#choiceA').text(questionObject.answers[0]);
-    $('#choiceB').text(questionObject.answers[1]);
-    $('#choiceC').text(questionObject.answers[2]);
-    $('#choiceD').text(questionObject.answers[3]);
-    
+    });
 
-    //qNumber++;
 }
 
 
 
-function finalResult () {
-    $('.startQuiz').hide();
-    $('.questionPage').hide();
-    $('.finalPage').removeClass('hide');
-}
+
+
+
+
+
+
+
+
+
 
 
 //Javascript Functions to run
 function begin() {
+    initializeQuiz();
     startPage();
     
-
+ //   next();
+ //   restartQuiz();
 }
 
 $(begin);
